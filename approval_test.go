d4cb26eab42bef6ac388eaebf9f3ccca24edf5d7
Nato Boram <nato.boram+git@gmail.com>
2024-09-15 00:43:54 -0400

    ✅ Fix Approval test

--- approval_test.go
+++ approval_test.go
@@ -7,25 +7,42 @@ import (
 )
 
 func TestUnmarshalApproval(t *testing.T) {
-	data := []byte(`{"explanation":"foo"}`)
+	data := []byte(`{"someone":"foo","something":"bar","consequences":"baz","explanation":"qux"}`)
 	approval, err := UnmarshalApproval(data)
 	if err != nil {
 		t.Fatalf("couldn't unmarshal an approval: %s", err)
 	}
 
-	if approval.Explanation != "foo" {
-		t.Fatalf("expected explanation to be 'foo', got %s", approval.Explanation)
+	if approval.Someone != "foo" {
+		t.Fatalf("expected someone to be %s, got %s", "foo", approval.Someone)
+	}
+
+	if approval.Something != "bar" {
+		t.Fatalf("expected something to be %s, got %s", "bar", approval.Something)
+	}
+
+	if approval.Consequences != "baz" {
+		t.Fatalf("expected consequences to be %s, got %s", "baz", approval.Consequences)
+	}
+
+	if approval.Explanation != "qux" {
+		t.Fatalf("expected explanation to be %s, got %s", "qux", approval.Explanation)
 	}
 }
 
 func TestMarshalApproval(t *testing.T) {
-	approval := Approval{Explanation: "foo"}
+	approval := Approval{
+		Someone:      "foo",
+		Something:    "bar",
+		Consequences: "baz",
+		Explanation:  "qux",
+	}
 	data, err := approval.Marshal()
 	if err != nil {
 		t.Fatalf("couldn't marshal an approval: %s", err)
 	}
 
-	expected := []byte(`{"explanation":"foo"}`)
+	expected := []byte(`{"someone":"foo","something":"bar","consequences":"baz","explanation":"qux"}`)
 	if string(data) != string(expected) {
 		t.Fatalf("expected %s, got %s", expected, data)
 	}

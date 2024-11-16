8f8684c03312beb2454674d6a19a01905466a1fe
Nato Boram <nato.boram+git@gmail.com>
2024-11-16 01:44:55 -0500

    ✅ TestMakeUserContext_NilOpReply

--- llm_test.go
+++ llm_test.go
@@ -7,20 +7,34 @@ import (
 	"github.com/Sadzeih/go-reddit/reddit"
 )
 
-func TestMakeUserContext(t *testing.T) {
+func TestMakeUserContext_WithOpReply(t *testing.T) {
 	post := &reddit.PostAndComments{
 		Post: &reddit.Post{
 			Title: "Test title",
 			URL:   "https://redd.it/lt8zlq",
 			Body:  "Test body",
 		},
-		Comments: []*reddit.Comment{},
-		More:     nil,
 	}
 	opReply := &reddit.Comment{Body: "Test comment"}
 	result := MakeUserContext(post, opReply)
 
 	expected := `<UserContext><PostTitle>Test title</PostTitle><PostUrl>https://redd.it/lt8zlq</PostUrl><PostBody>Test body</PostBody><ExplanatoryComment>Test comment</ExplanatoryComment></UserContext>`
+	if expected != result {
+		t.Errorf("expected %s, got %s", expected, result)
+	}
+}
+
+func TestMakeUserContext_NilOpReply(t *testing.T) {
+	post := &reddit.PostAndComments{
+		Post: &reddit.Post{
+			Title: "Test title",
+			URL:   "https://redd.it/lt8zlq",
+			Body:  "Test body",
+		},
+	}
+	result := MakeUserContext(post, nil)
+
+	expected := `<UserContext><PostTitle>Test title</PostTitle><PostUrl>https://redd.it/lt8zlq</PostUrl><PostBody>Test body</PostBody><ExplanatoryComment></ExplanatoryComment></UserContext>`
 	if expected != result {
 		t.Errorf("expected %s, got %s", expected, result)
 	}
